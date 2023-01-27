90e5446902f04de035ac56df50c2dadf7f043fa0
felipemaxplay <felipemaxplay@gmail.com>
2023-01-27 14:13:33 -0300

    game rules

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/data.json'
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+  finish: boolean = false;
   title: string = "";
   options: any;
   optionSelected: any;
@@ -13,7 +15,30 @@ export class QuizzComponent {
   answerSelected: string = '';
   questionIndex: number = 0;
   questionMaxIndex: number = 0;
-  finish: boolean = false;
 
+  ngOnInit(): void {
+    if(quizz_questions) {
+      this.finish = false;
+      this.title = quizz_questions.title;
+      this.options = quizz_questions.questions;
+      this.optionSelected = this.options[this.questionIndex];
 
+      this.questionMaxIndex = 0;
+      this.questionMaxIndex = this.options.length;
+    }
+  }
+
+  playerChoice(value: string): void {
+    this.answers.push(value);
+    console.log(this.answers);
+  }
+
+  nextStep(): void {
+    this.questionIndex += 1;
+    if(this.questionMaxIndex > this.questionIndex) {
+      this.optionSelected = this.options[this.questionIndex];
+    } else {
+      this.finish = true;
+    }
+  }
 }
